3111b7238572fa51414516c461d6d4c4bd888594
Mayank Bhardwaj <mayank85277@gmail.com>
2020-08-26 19:13:39 +0530

    cnmhm
    hj

--- version.cpp
+++ version.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main()
 {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
     cout<<__cplusplus;
     return 0;
 }
