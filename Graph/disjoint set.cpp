63222e4cba22cd41c209841bcbe725200852bce8
Mayank Bhardwaj <mayank85277@gmail.com>
2020-09-04 11:09:41 +0530

    commit

--- Graph/disjoint set.cpp
+++ Graph/disjoint set.cpp
@@ -5,14 +5,18 @@ using namespace std;
 struct DisjointSet
 {
     int v;
-    vector<int> parent,rank;
+    vector<int> parent,size;
     
     DisjointSet(int n)
     {
         this->v=n;
-        rank.resize(n+1,0);
+        size.resize(n+1);
         parent.resize(n+1);
-        for(int i=0;i<=n;i++) parent[i] = i;
+        for(int i=1;i<=n;i++)
+        {
+            size[i] = 1;
+            parent[i] = i;
+        } 
     }
 
     int find(int a)
@@ -21,21 +25,34 @@ struct DisjointSet
         return a;
     }
 
-    int merge(int a,int b)
+    void merge(int a,int b)
     {
         a = find(a);
         b = find(b);
-
-        if(rank[a] < rank[b]) swap(a,b);
+        if(a==b) return;
+        if(size[a] < size[b]) swap(a,b);
 
         parent[b] = a;
-        rank[a] += rank[b];
+        size[a] += size[b];
+    }
+
+    void print()
+    {
+        for(int i=1;i<=v;i++)
+        {
+            cout<<i<<" "<<parent[i]<<" "<<size[i]<<endl;
+        }
     }
 };
 
 int main()
 {
     DisjointSet ds(4);
-    
+    ds.merge(1,2);
+    ds.merge(1,3);
+    ds.merge(2,3);
+    // ds.merge(1,2);
+    // ds.merge(2,3);
+    ds.print();
     return 0;
 }
