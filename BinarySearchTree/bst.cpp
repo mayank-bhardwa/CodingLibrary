c6ec5fad41c214dfee82daff6531909c8a6cbed9
Mayank Bhardwaj <mayank85277@gmail.com>
2020-12-14 01:20:01 +0530

    trtgbdf

--- BinarySearchTree/bst.cpp
+++ BinarySearchTree/bst.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
 
-
 /*
     Operations On Binary Search Tree
     Floor
@@ -15,114 +14,121 @@ using namespace std;
 template <class T>
 class BST
 {
-    protected :
-        struct Node
+protected:
+    struct Node
+    {
+        T data;
+        struct Node *left, *right;
+    };
+    Node *root;
+
+public:
+    void insert(T data)
+    {
+        root = insert(root, data);
+    }
+    void inorder()
+    {
+        inorder(root);
+    }
+    void Delete(T data)
+    {
+        root = Delete(root, data);
+    }
+    BST()
+    {
+        root = NULL;
+    }
+    bool search(T data)
+    {
+        return search(root, data);
+    }
+
+private:
+    Node *newNode(T data)
+    {
+        Node *temp = new Node;
+        temp->data = data;
+        temp->left = NULL;
+        temp->right = NULL;
+        return temp;
+    }
+    Node *insert(Node *root, T data)
+    {
+        if (root == NULL)
+            return newNode(data);
+        else if (root->data > data)
+            root->left = insert(root->left, data);
+        else if (root->data < data)
+            root->right = insert(root->right, data);
+        return root;
+    }
+    bool search(Node *root, T data)
+    {
+        if (root == NULL)
+            return false;
+        else if (root->data == data)
+            return true;
+        else if (root->data > data)
+            return search(root->left, data);
+        else
+            return search(root->right, data);
+    }
+    void inorder(Node *root)
+    {
+        if (root)
         {
-            T data;
-            struct Node *left,*right; 
-        };
-        Node *root;
-    public : 
-        void insert(T data)  
-        {
-            root=insert(root,data);
+            inorder(root->left);
+            cout << root->data << " ";
+            inorder(root->right);
         }
-        void inorder()
-        {
-            inorder(root);
-        }
-        void Delete(T data)
-        {
-            root=Delete(root,data);
-        }
-        BST()
-        {
-            root=NULL;
-        }
-        bool search(T data)
-        {
-            return search(root,data);
-        }
-    private :
-        Node* newNode(T data)
-        {
-            Node *temp=new Node;
-            temp->data=data;
-            temp->left=NULL;
-            temp->right=NULL;
-            return temp;
-        }
-        Node* insert(Node *root,T data)
-        {
-            if(root==NULL)  return  newNode(data);
-            else if( root->data > data )
-                root->left=insert(root->left,data);
-            else if( root->data < data )
-                root->right=insert(root->right,data);
+    }
+    Node *inorderSuccessor(Node *node)
+    {
+        Node *temp = node;
+        while (temp && temp->left)
+            temp = temp->left;
+        return temp;
+    }
+    Node *Delete(Node *root, T data)
+    {
+        if (root == NULL)
             return root;
-        }
-        bool search(Node *root,T data)
+        else if (root->data > data)
+            root->left = Delete(root->left, data);
+        else if (root->data < data)
+            root->right = Delete(root->right, data);
+        else
         {
-            if(root==NULL) 
-                return false;
-            else if(root->data==data)
-                return true;
-            else if(root->data > data )
-                return search(root->left,data);
-            else 
-                return search(root->right,data);
-        }
-        void inorder(Node *root)
-        {
-            if(root)
+            if (root->left && root->right)
             {
-                inorder(root->left);
-                cout<<root->data<<" ";
-                inorder(root->right);
+                Node *temp = inorderSuccessor(root->right);
+                root->data = temp->data;
+                root->right = Delete(root->right, temp->data);
+            }
+            else if (root->left)
+            {
+                Node *temp = root->left;
+                delete root;
+                return temp;
+            }
+            else
+            {
+                Node *temp = root->right;
+                delete root;
+                return temp;
             }
         }
-        Node* inorderSuccessor(Node *node)
-        {
-            Node *temp=node;
-            while(temp && temp->left)temp=temp->left;
-            return temp;
-        }
-        Node* Delete(Node *root,T data)
-        {
-            if(root==NULL)return root;
-            else if ( root->data > data ) root->left = Delete(root->left,data);
-            else if ( root->data < data ) root->right = Delete(root->right,data);
-            else 
-            {
-                if( root->left && root->right )
-                {
-                    Node *temp = inorderSuccessor(root->right);
-                    root->data = temp->data;
-                    root->right = Delete(root->right,temp->data);
-                }
-                else if( root->left )
-                {
-                    Node *temp = root->left;
-                    delete root;
-                    return temp;
-                }
-                else
-                {
-                    Node *temp = root->right;
-                    delete root;
-                    return temp;
-                }
-            }
-            return root;
-        }
+        return root;
+    }
 };
 
 int main(int argc, char const *argv[])
 {
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt","r",stdin);
-        freopen("output.txt","w",stdout);
-    #endif
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
     BST<int> btree;
     btree.insert(50);
     btree.insert(30);
@@ -133,14 +139,15 @@ int main(int argc, char const *argv[])
     btree.insert(80);
     btree.inorder();
     btree.Delete(20);
-    cout<<"\n";
+    cout << "\n";
     btree.inorder();
     btree.Delete(30);
-    cout<<"\n";
+    cout << "\n";
     btree.inorder();
     btree.Delete(50);
-    cout<<"\n";
+    cout << "\n";
     btree.inorder();
-    cout<<"\n"<<btree.search(60)<<" "<<btree.search(20);
+    cout << "\n"
+         << btree.search(60) << " " << btree.search(20);
     return 0;
 }
