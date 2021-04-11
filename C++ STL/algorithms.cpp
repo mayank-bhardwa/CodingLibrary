4c2b56098ba6078ab72ed2b755c31f303667269b
Mayank Bhardwaj <mayank85277@gmail.com>
2021-04-12 00:04:35 +0530

    added documentation

--- C++ STL/algorithms.cpp
+++ C++ STL/algorithms.cpp
@@ -13,11 +13,11 @@ void print(T a)
 
 int main()
 {
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt","r",stdin);
-        freopen("output.txt","w",stdout);
-        freopen("error.txt","w",stderr);
-    #endif
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt","r",stdin);
+    //     freopen("output.txt","w",stdout);
+    //     freopen("error.txt","w",stderr);
+    // #endif
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);cout.tie(NULL);
 
